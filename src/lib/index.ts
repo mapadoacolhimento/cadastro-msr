2d2e7b452f5bbd04d97a0e9c04326324c18080cc
dieh <dri.leela@gmail.com>
2024-08-08 09:00:39 -0300

    feat: remove updateManyTickets

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -3,7 +3,6 @@ export { default as db } from "./db";
 export { default as getErrorMessage } from "./getErrorMessage";
 export { default as normalizeCity } from "./normalizeCity";
 export { default as formatZipcode } from "./formatZipcode";
-export { default as updateManyTickets } from "./zendesk/updateManyTickets";
 export { default as createOrUpdateUser } from "./zendesk/createOrUpdateUser";
 export { default as createOrUpdateTicket } from "./zendesk/createOrUpdateTicket";
 export * from "./constants";

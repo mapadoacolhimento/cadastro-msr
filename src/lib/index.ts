24de8a7c250a9a41dbb85b61e8d89a0837f90221
Viviane Dias <vivi@mapa.org.br>
2024-08-13 18:01:38 -0300

    feat: moves isDateValid func to /lib

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,7 @@
 export { default as sleep } from "./sleep";
 export { default as db } from "./db";
 export { default as getErrorMessage } from "./getErrorMessage";
+export { default as isDateValid } from "./isDateValid";
 export { default as updateManyTickets } from "./zendesk/updateManyTickets";
 export * from "./constants";
 export * from "./types";
---
+++ src/lib/isDateValid.ts
@@ -0,0 +1,10 @@
+export default function isDateValid(dateString: string) {
+	const [day, month, year] = dateString.split("/").map(Number);
+	const date = new Date(year, month - 1, day);
+	return (
+		!isNaN(date.getTime()) &&
+		date.getDate() === day &&
+		date.getMonth() === month - 1 &&
+		date.getFullYear() === year
+	);
+}

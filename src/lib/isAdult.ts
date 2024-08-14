7f64644154258495f1a07f1bb59e092ee7198b9a
gloria <gloriateodoro1@gmail.com>
2024-08-14 10:37:05 -0300

    feat: add day difference in birthday check for isAdult function

--- src/lib/isAdult.ts
+++ src/lib/isAdult.ts
@@ -5,15 +5,17 @@ export const isAdult = (dateOfBirth: string) => {
 	const birthDate = new Date(year, month - 1, day);
 	let ageDifference = today.getFullYear() - birthDate.getFullYear();
 	const monthDifference = today.getMonth() - birthDate.getMonth();
+	const dayDifference = today.getDate() - birthDate.getDate();
 
 	const hasNotReachedBirthdayMonth = monthDifference < 0;
 	const isBirthdayMonth = monthDifference === 0;
+	const hasNotReachedBirthdayDay = dayDifference < 0;
 	const hasNotPassedBirthdayThisYear =
-		hasNotReachedBirthdayMonth ||
-		(isBirthdayMonth && today.getDate() >= birthDate.getDate());
+		hasNotReachedBirthdayMonth || (isBirthdayMonth && hasNotReachedBirthdayDay);
 
 	if (hasNotPassedBirthdayThisYear) {
 		ageDifference--;
+	} else {
+		return ageDifference >= 18;
 	}
-	return ageDifference >= 18;
 };

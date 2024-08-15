9e696f6d50f8ef50937249b2177ca6f2155274bd
gloria <gloriateodoro1@gmail.com>
2024-08-15 09:53:04 -0300

    feat: add safe exit button

---
+++ src/components/SafeExitButton.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Flex, Button } from "@radix-ui/themes";
+import Image from "next/image";
+
+const handleClick = () => {
+	window.location.href = "https://www.google.com";
+};
+
+const SafeExitButton = () => (
+	<Flex
+		display={{ initial: "none", md: "flex" }}
+		justify={"end"}
+		mt={"7"}
+		mr={"4"}
+	>
+		<Button color="pink" size={"4"} onClick={handleClick}>
+			<Image
+				src="/icons/return.svg"
+				alt="ícone de seta de retorno"
+				width={"18"}
+				height={"18"}
+			/>
+			Sair desse site
+		</Button>
+	</Flex>
+);
+
+export default SafeExitButton;

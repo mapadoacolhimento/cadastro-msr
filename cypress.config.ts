f064e74a5dc1d049c154779960610e6c23d87518
Viviane Dias <vivi@mapa.org.br>
2024-08-18 12:36:03 -0300

    test: configures axe in cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,5 +3,19 @@ import { defineConfig } from "cypress";
 export default defineConfig({
 	e2e: {
 		baseUrl: "http://localhost:3000",
+		setupNodeEvents(on) {
+			on("task", {
+				log(message) {
+					console.log(message);
+
+					return null;
+				},
+				table(message) {
+					console.table(message);
+
+					return null;
+				},
+			});
+		},
 	},
 });

9b5136e177ec466c4e11c85f3afb0e723f411f5f
dieh <dri.leela@gmail.com>
2024-08-27 18:23:11 -0300

    feat(integration-tests): add config and method to add support requests

---
+++ src/tests/integration/helpers/insert-support-requests.ts
@@ -0,0 +1,64 @@
+import { db } from "@/lib";
+import {
+	THERAPIST_ZENDESK_USER_ID,
+	LAWYER_ZENDESK_USER_ID,
+	MSR_ZENDESK_USER_ID,
+} from "./constants";
+import {
+	MatchStatus,
+	SupportRequestsStatus,
+	SupportType,
+	SupportRequests,
+} from "@prisma/client";
+
+type SupportRequestsInfo = {
+	status: SupportRequestsStatus;
+	supportType: SupportType;
+	zendeskTicketId: SupportRequests["zendeskTicketId"];
+	matchStatus?: MatchStatus;
+};
+
+export default async function insertSupportRequests(
+	supportRequests: SupportRequestsInfo[]
+) {
+	for (let i = 0; supportRequests.length > i; i++) {
+		const { zendeskTicketId, supportType, status, matchStatus } =
+			supportRequests[i];
+		const supportRequest = await db.supportRequests.create({
+			data: {
+				msrId: MSR_ZENDESK_USER_ID,
+				zendeskTicketId: zendeskTicketId,
+				supportType: supportType,
+				priority: null,
+				supportExpertise: null,
+				hasDisability: null,
+				requiresLibras: null,
+				acceptsOnlineSupport: true,
+				lat: -23.55242,
+				lng: -46.65735,
+				city: "SAO PAULO",
+				state: "SP",
+				status: status,
+			},
+		});
+
+		if (matchStatus) {
+			await db.matches.create({
+				data: {
+					msrId: MSR_ZENDESK_USER_ID,
+					volunteerId:
+						supportType === "legal"
+							? LAWYER_ZENDESK_USER_ID
+							: THERAPIST_ZENDESK_USER_ID,
+					msrZendeskTicketId: zendeskTicketId,
+					volunteerZendeskTicketId: Math.random(),
+					supportType: supportType,
+					matchType: "msr",
+					matchStage: "ideal",
+					status: matchStatus,
+					supportRequestId: supportRequest.supportRequestId,
+				},
+			});
+		}
+	}
+}
--- src/tests/integration/helpers/reset-db.ts
+++ src/tests/integration/helpers/reset-db.ts
@@ -1,9 +1,13 @@
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
+import { db } from "@/lib";
 
 export default async () => {
-	await prisma.$transaction([
-		// prisma.user.deleteMany()
+	await db.$transaction([
+		db.mSRPiiSec.deleteMany(),
+		db.mSRStatusHistory.deleteMany(),
+		db.mSRs.deleteMany(),
+		db.matches.deleteMany(),
+		db.supportRequestStatusHistory.deleteMany(),
+		db.supportRequests.deleteMany(),
+		db.volunteers.deleteMany(),
 	]);
 };

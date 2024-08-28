885fb29030afc56ac362675d86b1f961653450c2
Viviane Dias <vivi@mapa.org.br>
2024-08-28 11:45:18 -0300

    tests(setup): adds name to resetDb function

--- src/tests/integration/helpers/reset-db.ts
+++ src/tests/integration/helpers/reset-db.ts
@@ -1,13 +1,13 @@
 import { db } from "@/lib";
 
-export default async () => {
+export default async function resetDb() {
 	await db.$transaction([
-		db.mSRPiiSec.deleteMany(),
 		db.mSRStatusHistory.deleteMany(),
+		db.mSRPiiSec.deleteMany(),
 		db.mSRs.deleteMany(),
 		db.matches.deleteMany(),
 		db.supportRequestStatusHistory.deleteMany(),
 		db.supportRequests.deleteMany(),
 		db.volunteers.deleteMany(),
 	]);
-};
+}

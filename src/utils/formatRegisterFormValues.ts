d54cfdaac9f75c2d35302924556535618c59cd4b
Viviane Dias <vivi@mapa.org.br>
2024-09-09 15:14:04 -0300

    fix: uses enum from prisma to set form options

--- src/utils/formatRegisterFormValues.ts
+++ src/utils/formatRegisterFormValues.ts
@@ -1,6 +1,8 @@
 import { Values } from "@/types";
 import getFirstName from "./getFirstName";
 import capitalizeFirst from "./capitalizeFirst";
+import formatZipcode from "./formatZipcode";
+import normalizeCity from "./normalizeCity";
 
 export default function formatRegisterFormValues(values: Values) {
 	const parseValues = {
@@ -8,6 +10,9 @@ export default function formatRegisterFormValues(values: Values) {
 		firstName: getFirstName(values.firstName),
 		neighborhood: capitalizeFirst(values.neighborhood),
 		dateOfBirth: values.dateOfBirth.replaceAll("/", "-"),
+		zipcode: formatZipcode(values.zipcode),
+		phone: values.phone.replace(/\D/g, ""),
+		city: normalizeCity(values.city),
 	};
 
 	const replaceYesNoWithBoolean = JSON.stringify(parseValues)

610ee6b0fc268f28455fd2d6c90fc0dd3d96a63a
Viviane Dias <vivi@mapa.org.br>
2024-09-10 15:59:47 -0300

    fix: date formatter not transform in number before formatting to dash

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,7 +1,7 @@
 export default function formatDate(date: string): string {
 	if (!date) return "";
 
-	const [day, month, year] = date.split("/").map(Number);
+	const [day, month, year] = date.split("/");
 
 	return `${year}-${month}-${day}`;
 }

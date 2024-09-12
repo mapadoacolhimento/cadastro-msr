7e3ae8627f00883a67a2e6c00f2bb5393bf3397d
Viviane Dias <vivi@mapa.org.br>
2024-09-12 10:28:10 -0300

    chore: adds new relic monitoring

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,17 @@
+"use strict";
+
 /** @type {import('next').NextConfig} */
+const nrExternals = require("@newrelic/next/load-externals");
+
 const nextConfig = {
 	reactStrictMode: true,
+	experimental: {
+		serverComponentsExternalPackages: ["newrelic"],
+	},
+	webpack: (config) => {
+		nrExternals(config);
+		return config;
+	},
 };
 
 export default nextConfig;

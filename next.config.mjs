c0f10fad2f53aeb8a76c5b52cded9344dbe30e01
Viviane Dias <vivi@mapa.org.br>
2024-09-17 10:36:20 -0300

    chore: adds security headers to all paths

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,31 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
+	async headers() {
+		return [
+			{
+				source: "/(.*)",
+				headers: [
+					{
+						key: "X-Content-Type-Options",
+						value: "nosniff",
+					},
+					{
+						key: "X-Frame-Options",
+						value: "SAMEORIGIN",
+					},
+					{
+						key: "Referrer-Policy",
+						value: "same-origin",
+					},
+					{
+						key: "Permissions-Policy",
+						value: "microphone=(), geolocation=(), camera=()",
+					},
+				],
+			},
+		];
+	},
 };
 
 export default nextConfig;

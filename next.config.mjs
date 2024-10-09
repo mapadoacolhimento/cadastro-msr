b3e0094b2776bf3e87de4f8d79b7556a9d5a3155
dieh <dri.leela@gmail.com>
2024-10-09 15:12:22 -0300

    feat add plugin prisma/nextjs-monorepo-workaround-plugin

--- next.config.mjs
+++ next.config.mjs
@@ -2,14 +2,18 @@
 
 /** @type {import('next').NextConfig} */
 import nrExternals from "newrelic/load-externals.js";
+import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
 
 const nextConfig = {
 	reactStrictMode: true,
 	experimental: {
 		serverComponentsExternalPackages: ["newrelic"],
 	},
-	webpack: (config) => {
+	webpack: (config, { isServer }) => {
 		nrExternals(config);
+		if (isServer) {
+			config.plugins = [...config.plugins, new PrismaPlugin()]
+		}
 		return config;
 	},
 };

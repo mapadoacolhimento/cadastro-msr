54eb4b5e699951b791acd54cfc260030d822f937
Viviane Dias <vivi@mapa.org.br>
2025-01-02 19:58:40 -0300

    feat: improves page performance

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -47,6 +47,7 @@ export default async function RootLayout({
 			<Script
 				id="nr-browser-agent"
 				dangerouslySetInnerHTML={{ __html: browserTimingHeader }}
+				strategy="lazyOnload"
 			/>
 			<body>
 				<Providers>

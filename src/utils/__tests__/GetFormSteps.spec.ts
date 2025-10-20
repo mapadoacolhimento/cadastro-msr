9e343d5d6406a753452f407e4546f45d6be189dd
Janaína Pessanha <janainapessanha@Janainas-MacBook.local>
2025-10-20 11:49:45 -0300

    edit unit test

--- src/utils/__tests__/GetFormSteps.spec.ts
+++ src/utils/__tests__/GetFormSteps.spec.ts
@@ -1,12 +1,13 @@
 import { describe, it, expect, vi } from "vitest";
 import { getFormSteps, getHiddenStepsForm } from "@/utils/getFormSteps";
 
+import { ViolenceTime } from "../../components/MultiStepForm/Steps";
+
 describe("MultiStepForm - Steps Logic", () => {
 	describe("getFormSteps function", () => {
 		it("should return 11 steps without ViolenceTime", () => {
 			const steps = getFormSteps();
 			expect(steps).toHaveLength(11);
-			expect(steps).not.toContain("ViolenceTime");
 		});
 	});
 
@@ -14,10 +15,8 @@ describe("MultiStepForm - Steps Logic", () => {
 		it("should return an array with one element containing 12 steps including ViolenceTime", () => {
 			const result = getHiddenStepsForm();
 
-			expect(result).toHaveLength(1);
-
-			const stepsArray = result[0];
-			expect(stepsArray).toHaveLength(12);
+			expect(result).toHaveLength(12);
+			expect(result[11]).toStrictEqual(ViolenceTime());
 		});
 	});
 });

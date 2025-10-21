0373cdd0d032f3a8d5be77c53ec29aac5ab8c88a
Janaína Pessanha <janainapessanha@Janainas-MacBook.local>
2025-10-21 17:43:32 -0300

    comment out app2 e2e test

--- cypress/e2e/App2.cy.ts
+++ cypress/e2e/App2.cy.ts
@@ -1,172 +1,172 @@
-import userData from "../fixtures/userData.json";
+//TODO: arquivo deve ser descomentado uma vez que todos os passos do novo cadastro estejam implementados. Até lá, é possível descomentar o teste e usá-lo
+//localmente alterando o valor da variável de ambiente setada no cypress.config
 
-const {
-	gender,
-	externalSupport,
-	violenceLocation,
-	violenceTime,
-	dateOfBirth,
-	supportTypes,
-} = userData;
+// import userData from "../fixtures/userData.json";
 
-describe("Happy path", () => {
-	beforeEach(() => {
-		cy.intercept("POST", "/handle-request", {
-			statusCode: 200,
-			body: {
-				psychological: { supportRequestId: 1 },
-				legal: { supportRequestId: 2 },
-			},
-		}).as("submitRegistration");
-	});
+// const {
+// 	gender,
+// 	externalSupport,
+// 	violenceLocation,
+// 	violenceTime,
+// 	dateOfBirth,
+// 	supportTypes,
+// } = userData;
 
-	describe("New Steps added (Desktop)", () => {
-		before(() => {
-			Cypress.env("NEXT_PUBLIC_ENABLE_NEW_STEPS", true);
-		});
+// describe("Happy path", () => {
+// 	beforeEach(() => {
+// 		cy.intercept("POST", "/handle-request", {
+// 			statusCode: 200,
+// 			body: {
+// 				psychological: { supportRequestId: 1 },
+// 				legal: { supportRequestId: 2 },
+// 			},
+// 		}).as("submitRegistration");
+// 	});
 
-		beforeEach(() => {
-			cy.viewport(1920, 1080);
-		});
+// 	describe("New Steps added (Desktop)", () => {
 
-		it("should successfully submit the form", () => {
-			cy.visit("/cadastro");
+// 		beforeEach(() => {
+// 			cy.viewport(1920, 1080);
+// 		});
 
-			cy.fillGenderIdentityStep(gender);
-			cy.findByRole("button", { name: "Continuar" }).click();
+// 		it("should successfully submit the form", () => {
+// 			cy.visit("/cadastro");
 
-			cy.fillDateOfBirthStep(dateOfBirth);
-			cy.findByRole("button", { name: "Continuar" }).click();
+// 			cy.fillGenderIdentityStep(gender);
+// 			cy.findByRole("button", { name: "Continuar" }).click();
 
-			cy.fillViolenceTypeStep();
-			cy.findByRole("button", { name: "Continuar" }).click();
+// 			cy.fillDateOfBirthStep(dateOfBirth);
+// 			cy.findByRole("button", { name: "Continuar" }).click();
 
-			cy.fillViolenceLocationStep(violenceLocation);
-			cy.findByRole("button", { name: "Continuar" }).click();
+// 			cy.fillViolenceTypeStep();
+// 			cy.findByRole("button", { name: "Continuar" }).click();
 
-			cy.fillExternalSupportStep(externalSupport);
-			cy.findByRole("button", { name: "Continuar" }).click();
+// 			cy.fillViolenceLocationStep(violenceLocation);
+// 			cy.findByRole("button", { name: "Continuar" }).click();
 
-			cy.fillFinancialBlock();
-			cy.findByRole("button", { name: "Continuar" }).click();
+// 			cy.fillExternalSupportStep(externalSupport);
+// 			cy.findByRole("button", { name: "Continuar" }).click();
 
-			cy.goThroughBeginRegistrationStep();
-			cy.findByRole("button", { name: "Iniciar cadastro" }).click();
+// 			cy.fillFinancialBlock();
+// 			cy.findByRole("button", { name: "Continuar" }).click();
 
-			cy.fillSupportTypeStep(supportTypes);
-			cy.findByRole("button", { name: "Continuar" }).click();
+// 			cy.goThroughBeginRegistrationStep();
+// 			cy.findByRole("button", { name: "Iniciar cadastro" }).click();
 
-			cy.fillBasicRegisterInformationStep();
-			cy.findByRole("button", { name: "Continuar" }).click();
+// 			cy.fillSupportTypeStep(supportTypes);
+// 			cy.findByRole("button", { name: "Continuar" }).click();
 
-			cy.fillGeolocationStep();
-			cy.findByRole("button", { name: "Continuar" }).click();
+// 			cy.fillBasicRegisterInformationStep();
+// 			cy.findByRole("button", { name: "Continuar" }).click();
 
-			cy.fillDiversityInformationStep();
-			cy.findByRole("button", { name: "Continuar" }).click();
+// 			cy.fillGeolocationStep();
+// 			cy.findByRole("button", { name: "Continuar" }).click();
 
-			cy.fillViolenceTimeStep();
-			cy.findByRole("radio", { name: violenceTime.isolatedEpisode }).click();
-			cy.findByRole("button", { name: "Enviar" }).click();
+// 			cy.fillDiversityInformationStep();
+// 			cy.findByRole("button", { name: "Continuar" }).click();
 
-			cy.wait("@submitRegistration");
-			cy.contains("Cadastro realizado").should("be.visible");
-		});
+// 			cy.fillViolenceTimeStep();
+// 			cy.findByRole("radio", { name: violenceTime.isolatedEpisode }).click();
+// 			cy.findByRole("button", { name: "Enviar" }).click();
 
-		it("should go back to the previous step when the back button is clicked", () => {
-			cy.visit("/cadastro");
+// 			cy.wait("@submitRegistration");
+// 			cy.contains("Cadastro realizado").should("be.visible");
+// 		});
 
-			cy.fillGenderIdentityStep(gender);
-			cy.findByRole("button", { name: "Continuar" }).click();
+// 		it("should go back to the previous step when the back button is clicked", () => {
+// 			cy.visit("/cadastro");
 
-			cy.fillDateOfBirthStep(dateOfBirth);
-			cy.findByRole("button", { name: "Continuar" }).click();
+// 			cy.fillGenderIdentityStep(gender);
+// 			cy.findByRole("button", { name: "Continuar" }).click();
 
-			cy.fillViolenceTypeStep();
-			cy.findByRole("button", { name: "Continuar" }).click();
+// 			cy.fillDateOfBirthStep(dateOfBirth);
+// 			cy.findByRole("button", { name: "Continuar" }).click();
 
-			cy.fillViolenceLocationStep(violenceLocation);
-			cy.findByRole("button", { name: "Continuar" }).click();
+// 			cy.fillViolenceTypeStep();
+// 			cy.findByRole("button", { name: "Continuar" }).click();
 
-			cy.fillExternalSupportStep(externalSupport);
-			cy.findByRole("button", { name: "Continuar" }).click();
+// 			cy.fillViolenceLocationStep(violenceLocation);
+// 			cy.findByRole("button", { name: "Continuar" }).click();
 
-			cy.fillFinancialBlock();
-			cy.findByRole("button", { name: "Continuar" }).click();
+// 			cy.fillExternalSupportStep(externalSupport);
+// 			cy.findByRole("button", { name: "Continuar" }).click();
 
-			cy.goThroughBeginRegistrationStep();
-			cy.findByRole("button", { name: "Iniciar cadastro" }).click();
+// 			cy.fillFinancialBlock();
+// 			cy.findByRole("button", { name: "Continuar" }).click();
 
-			cy.fillSupportTypeStep(supportTypes);
-			cy.findByRole("button", { name: "Continuar" }).click();
+// 			cy.goThroughBeginRegistrationStep();
+// 			cy.findByRole("button", { name: "Iniciar cadastro" }).click();
 
-			cy.fillBasicRegisterInformationStep();
-			cy.findByRole("button", { name: "Continuar" }).click();
+// 			cy.fillSupportTypeStep(supportTypes);
+// 			cy.findByRole("button", { name: "Continuar" }).click();
 
-			cy.fillGeolocationStep();
-			cy.findByRole("button", { name: "Continuar" }).click();
+// 			cy.fillBasicRegisterInformationStep();
+// 			cy.findByRole("button", { name: "Continuar" }).click();
 
-			cy.fillDiversityInformationStep();
-			cy.findByRole("button", { name: "Continuar" }).click();
+// 			cy.fillGeolocationStep();
+// 			cy.findByRole("button", { name: "Continuar" }).click();
 
-			cy.fillViolenceTimeStep();
+// 			cy.fillDiversityInformationStep();
+// 			cy.findByRole("button", { name: "Continuar" }).click();
 
-			cy.findByRole("button", { name: "Voltar para o passo anterior" }).click();
-			cy.contains("Seus dados").should("exist");
-		});
-	});
+// 			cy.fillViolenceTimeStep();
 
-	describe("Mobile", () => {
-		beforeEach(() => {
-			cy.viewport(390, 844);
-			Cypress.env("NEXT_PUBLIC_ENABLE_NEW_STEPS", true);
-		});
+// 			cy.findByRole("button", { name: "Voltar para o passo anterior" }).click();
+// 			cy.contains("Seus dados").should("exist");
+// 		});
+// 	});
 
-		it("should successfully submit the form after filling the Violence Time step", () => {
-			cy.visit("/cadastro");
+// 	describe("Mobile", () => {
+// 		before(() => {
+// 			cy.viewport(390, 844);
+// 			Cypress.env("NEXT_PUBLIC_ENABLE_NEW_STEPS", true);
+// 		});
 
-			cy.fillGenderIdentityStep(gender);
-			cy.findByRole("button", { name: "Continuar" }).click();
+// 		it("should successfully submit the form after filling the Violence Time step", () => {
+// 			cy.visit("/cadastro");
 
-			cy.fillDateOfBirthStep(dateOfBirth);
-			cy.findByRole("button", { name: "Continuar" }).click();
+// 			cy.fillGenderIdentityStep(gender);
+// 			cy.findByRole("button", { name: "Continuar" }).click();
 
-			cy.fillViolenceTypeStep();
-			cy.findByRole("button", { name: "Continuar" }).click();
+// 			cy.fillDateOfBirthStep(dateOfBirth);
+// 			cy.findByRole("button", { name: "Continuar" }).click();
 
-			cy.fillViolenceLocationStep(violenceLocation);
-			cy.findByRole("button", { name: "Continuar" }).click();
+// 			cy.fillViolenceTypeStep();
+// 			cy.findByRole("button", { name: "Continuar" }).click();
 
-			cy.fillExternalSupportStep(externalSupport);
-			cy.findByRole("button", { name: "Continuar" }).click();
+// 			cy.fillViolenceLocationStep(violenceLocation);
+// 			cy.findByRole("button", { name: "Continuar" }).click();
 
-			cy.fillFinancialBlock();
-			cy.findByRole("button", { name: "Continuar" }).click();
+// 			cy.fillExternalSupportStep(externalSupport);
+// 			cy.findByRole("button", { name: "Continuar" }).click();
 
-			cy.goThroughBeginRegistrationStep();
-			cy.findByRole("button", { name: "Iniciar cadastro" }).click();
+// 			cy.fillFinancialBlock();
+// 			cy.findByRole("button", { name: "Continuar" }).click();
 
-			cy.fillSupportTypeStep(supportTypes);
-			cy.findByRole("button", { name: "Continuar" }).click();
+// 			cy.goThroughBeginRegistrationStep();
+// 			cy.findByRole("button", { name: "Iniciar cadastro" }).click();
 
-			cy.fillBasicRegisterInformationStep();
-			cy.findByRole("button", { name: "Continuar" }).click();
+// 			cy.fillSupportTypeStep(supportTypes);
+// 			cy.findByRole("button", { name: "Continuar" }).click();
 
-			cy.fillGeolocationStep();
-			cy.findByRole("button", { name: "Continuar" }).click();
+// 			cy.fillBasicRegisterInformationStep();
+// 			cy.findByRole("button", { name: "Continuar" }).click();
 
-			cy.fillDiversityInformationStep();
-			cy.findByRole("button", { name: "Continuar" }).click();
+// 			cy.fillGeolocationStep();
+// 			cy.findByRole("button", { name: "Continuar" }).click();
 
-			cy.contains(
-				"Por quanto tempo você sofreu ou tem sofrido violência?"
-			).should("exist");
+// 			cy.fillDiversityInformationStep();
+// 			cy.findByRole("button", { name: "Continuar" }).click();
 
-			cy.findByRole("radio", { name: violenceTime.isolatedEpisode }).click();
-			cy.findByRole("button", { name: "Enviar" }).click();
+// 			cy.contains(
+// 				"Por quanto tempo você sofreu ou tem sofrido violência?"
+// 			).should("exist");
 
-			cy.wait("@submitRegistration");
-			cy.contains("Cadastro realizado").should("be.visible");
-		});
-	});
-});
+// 			cy.findByRole("radio", { name: violenceTime.isolatedEpisode }).click();
+// 			cy.findByRole("button", { name: "Enviar" }).click();
+
+// 			cy.wait("@submitRegistration");
+// 			cy.contains("Cadastro realizado").should("be.visible");
+// 		});
+// 	});
+// });

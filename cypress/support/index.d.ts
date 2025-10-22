b54ac710f4702b38bb5529027774f7bb47917313
Janaína Pessanha <janainapessanha@Janainas-MacBook.local>
2025-10-22 11:50:46 -0300

    add fillViolenceGenderIdStep to cypress index

--- cypress/support/index.d.ts
+++ cypress/support/index.d.ts
@@ -36,5 +36,6 @@ declare namespace Cypress {
 		fillViolenceTypeStep(): Chainable<any>;
 		fillNoViolenceTypeStep(): Chainable<any>;
 		fillViolenceTimeStep(): Chainable<any>;
+		fillViolenceGenderIdStep(violenceGenderId: string): Chainable<any>;
 	}
 }

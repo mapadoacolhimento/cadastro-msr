b7d0056a3117e719cd6d052185708501bb738103
Janaína Pessanha <janainapessanha@Janainas-MacBook.local>
2025-10-31 15:25:32 -0300

    add violence location to support e2e files

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -295,6 +295,16 @@ Cypress.Commands.add(
 	}
 );
 
+Cypress.Commands.add("fillViolenceLocationStep", () => {
+	cy.contains("Onde ocorreu a violência?").should("be.visible");
+	cy.findByRole("checkbox", { name: /Ambiente doméstico/i }).click({
+		force: true,
+	});
+	cy.findByRole("checkbox", { name: /Ambiente de trabalho/i }).click({
+		force: true,
+	});
+});
+
 Cypress.Commands.add("fillAllSteps", (supportTypes: Record<string, string>) => {
 	cy.fillGenderIdentityStep(gender);
 	cy.findByRole("button", { name: "Continuar" }).click();

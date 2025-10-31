f312e0134f4c43aaef21c264f5f8e0119adaecdf
Janaína Pessanha <janainapessanha@Janainas-MacBook.local>
2025-10-31 13:49:30 -0300

    create ViolenceLocation component

---
+++ src/components/MultiStepForm/Steps/ViolenceLocation.tsx
@@ -0,0 +1,36 @@
+import * as Yup from "yup";
+import { Strong } from "@radix-ui/themes";
+import Step from "../Step";
+import CheckboxGroupInput from "../../CheckboxGroupInput";
+import { violenceLocationOptions } from "@/constants";
+
+const violenceLocationSchema = Yup.object({
+	violenceLocationOptions: Yup.array()
+		.of(Yup.string().oneOf(violenceLocationOptions.map((o) => o.value)))
+		.min(1, "Selecione uma ou mais opções.")
+		.required("Selecione uma ou mais opções."),
+});
+
+export default function ViolenceLocation() {
+	return (
+		<Step
+			validationSchema={violenceLocationSchema}
+			title={"Dados da Violência"}
+			img={{
+				src: "/illustrations/woman-covering-ears.webp",
+				alt: "Ilustração de uma mulher de cabeça baixa tampando os ouvidos",
+			}}
+		>
+			<CheckboxGroupInput
+				name="violenceLocation"
+				options={violenceLocationOptions}
+				question={
+					<>
+						Onde <Strong>ocorreu</Strong> a violência?{" "}
+						<em>(Selecione todas as opções que se aplicam)</em>
+					</>
+				}
+			/>
+		</Step>
+	);
+}

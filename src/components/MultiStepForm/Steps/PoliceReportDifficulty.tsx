b73b83697b41b71f57cb15115b78cc430a91a929
Janaína Pessanha <janainapessanha@Janainas-MacBook.local>
2025-11-05 16:25:10 -0300

    insert difficulty options on component

--- src/components/MultiStepForm/Steps/PoliceReportDifficulty.tsx
+++ src/components/MultiStepForm/Steps/PoliceReportDifficulty.tsx
@@ -1,11 +1,13 @@
 import * as Yup from "yup";
+import { Strong } from "@radix-ui/themes";
 import Step from "../Step";
-import RadioInput from "../../RadioInput";
+import CheckboxGroupInput from "../../CheckboxGroupInput";
 import { policeReportDifficultyOptions } from "@/constants";
 
 const policeReportDifficultySchema = Yup.object({
-	policeReportDifficulty: Yup.string()
-		.oneOf(policeReportDifficultyOptions.map((a) => a.value))
+	policeReportDifficulty: Yup.array()
+		.of(Yup.string().oneOf(policeReportDifficultyOptions.map((a) => a.value)))
+		.min(1, "Selecione pelo menos uma opção.")
 		.required("Esse campo é obrigatório."),
 });
 
@@ -19,13 +21,15 @@ export default function PoliceReportDifficulty() {
 				alt: "Ilustração de uma mulher de cabeça baixa tampando os ouvidos",
 			}}
 		>
-			<RadioInput
+			<CheckboxGroupInput
 				name="policeReportDifficulty"
 				options={policeReportDifficultyOptions}
 				question={
 					<>
 						Houve dificuldade para solicitar medida protetiva e/ou denunciar
-						e/ou registrar um boletim de ocorrência?
+						e/ou registrar um boletim de ocorrência?{" "}
+						<Strong>O que a pessoa que te atendeu fez?</Strong>{" "}
+						<em>(Selecione todas as opções que se aplicam)</em>
 					</>
 				}
 			/>
